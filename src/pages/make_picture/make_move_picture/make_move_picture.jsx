8f1a44fd8394c54026655ab4ab7db923626ac412
Mason <liujianfeng@kingdee.com>
2021-01-06 15:10:35 +0800

    首页ui制作

--- src/pages/make_picture/make_move_picture/make_move_picture.jsx
+++ src/pages/make_picture/make_move_picture/make_move_picture.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Button, Canvas, CoverView } from '@tarojs/components'
+import { View, Button, Canvas, CoverView, Image } from '@tarojs/components'
 import { AtNavBar } from 'taro-ui'
 import Taro, { getImageInfo, createCanvasContext } from '@tarojs/taro'
 import GIF from 'gif.js'
@@ -49,45 +49,24 @@ export default function HomeList () {
     const handleUpload = () => {
         const ctx = createCanvasContext('myCanvas')
         Taro.chooseImage({
-            count: 9, // 默认9
+            count: 1, // 默认9
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有，在H5浏览器端支持使用 `user` 和 `environment`分别指定为前后摄像头
             success: async function (res) {
                 // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
                 var tempFilePaths = res.tempFilePaths
-                let x = 0, y = 0, heights = []
-                // const heights = tempFilePaths.map(item => width / item.width * item.height)
-                for (let i = 0; i < tempFilePaths.length; i++) {
-                    await getImageInfo({
-                        src: tempFilePaths[i],
-                        success: function (res) {
-                            const { width, height } = res
-                            const { sWidth, sHeight } = systemScreen
-                            const h = sWidth / width * height
-                            Taro.getSystemInfo
-                            ctx.drawImage(tempFilePaths[i], 0, y, sWidth, h)
-                            y += h
-                        }
-                    })
-                }
-                console.log(x, y)
-                setCanvasStyle({
-                    width: systemScreen.sWidth,
-                    height: y
-                })
-                ctx.draw(false, setTimeout(() => {
-                    Taro.canvasToTempFilePath({
-                        width: systemScreen.sWidth,
-                        height: y,
-                        destWidth: 2 * systemScreen.sWidth,
-                        destHeight: 2 * y,
-                        canvasId: 'myCanvas',
-                        success: function (res) {
-                            console.log(res)
-                            setImgUrl(res.tempFilePath)
-                        }
-                    })
-                }, 1000))
+                let img = document.createElement('img')
+                img.src = tempFilePaths[0]
+                img.style.width = '200px'
+                img.style.height = '200px'
+                console.log(img)
+                gif.addFrame(img, { delay: 20 });
+                gif.on('finished', function (blob) {
+                    console.log(blob)
+                    // window.open(URL.createObjectURL(blob));
+                });
+
+                gif.render();
             }
         })
     }
@@ -123,14 +102,13 @@ export default function HomeList () {
             <AtNavBar
                 onClickLeftIcon={handleClick}
                 color='#000'
-                title='长图制作'
+                title='动图制作'
                 leftText='返回'
             />
-            <Canvas
-                id="myCanvas"
-                canvasId='myCanvas'
-                style={canvasStyle}
-            ></Canvas>
+            <Image
+
+                style={{ width: 200, height: 200 }}
+            ></Image>
             <CoverView className='button_cotainer'>
                 <Button className='button_circle button_left' onClick={handleUpload}>上传</Button>
                 <Button className='button_circle button_right' onClick={handleSave}>保存</Button>
--- src/pages/make_picture/make_picture.jsx
+++ src/pages/make_picture/make_picture.jsx
@@ -1,27 +1,54 @@
 import React, { useRef, useState } from 'react'
 import { View, Text, Button } from '@tarojs/components'
+import { AtAvatar } from 'taro-ui'
 import Taro from '@tarojs/taro'
 
 import './index.less'
+import img1 from '../../assets/1.jpg'
+import img2 from '../../assets/2.jpg'
+import img3 from '../../assets/3.jpg'
+import img4 from '../../assets/4.jpg'
+import img5 from '../../assets/5.jpeg'
+import img6 from '../../assets/6.jpeg'
+
 
 export default function HomeList () {
 
     const handleClick = (e) => {
-        // e.persist()
-        // console.log(e)
-        // Taro.navigateTo({
-        //     url: `/pages/make_picture/${e._targetInst.key}/${e._targetInst.key}`
-        // })
         Taro.navigateTo({
             url: `/pages/make_picture/${e}/${e}`
         })
     }
 
     return (
-        <View className='make_picture_Container'>
-            <Button className='make_picture_button button1' onClick={() => handleClick('make_long_picture')}>制作长图</Button>
-            {/* <Button className='make_picture_button button2' onClick={() => handleClick('make_scrawl_picture')}>涂鸦</Button> */}
-            <Button className='make_picture_button button3' onClick={() => handleClick('make_move_picture')}>制作动图</Button>
+        <View>
+            <View className='make_picture_Container' />
+            <View className='button_container'>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_long_picture')}>
+                    <AtAvatar circle image={img1} />
+                    <Text>制作长图</Text>
+                </View>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_scrawl_picture')}>
+                    <AtAvatar circle image={img2} />
+                    <Text>涂鸦</Text>
+                </View>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_move_picture')}>
+                    <AtAvatar circle image={img3} />
+                    <Text>制作动图</Text>
+                </View>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_long_picture')}>
+                    <AtAvatar circle image={img4} />
+                    <Text>制作头像</Text>
+                </View>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_long_picture')}>
+                    <AtAvatar circle image={img5} />
+                    <Text>制作九宫格</Text>
+                </View>
+                <View className='make_picture_button button1' onClick={() => handleClick('make_long_picture')}>
+                    <AtAvatar circle image={img6} />
+                    <Text>制作长图</Text>
+                </View>
+            </View>
         </View>
     )
 }
